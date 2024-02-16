c943b699ea346fb24073882f974422556567f54f
Erik Bissell <erik@Eriks-Air.tbissell.net>
2024-02-16 12:04:26 -0500

    cleaning up code. adding comments. making more readable. Bishop.java

--- src/main/java/erikbissell/com/WebChess/Bishop.java
+++ src/main/java/erikbissell/com/WebChess/Bishop.java
@@ -6,97 +6,8 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Bishop extends Piece{
-
-    public Bishop(){
-
-    }
-    public Bishop(Bishop otherBishop){
-        setFile(otherBishop.getFile());
-        setRank(otherBishop.getRank());
-        setColor(otherBishop.isWhite());
-        setNickName('B');
-    }
-    public Bishop (int initialRank, int initialFile, boolean isWhite){
-        super(initialRank, initialFile, isWhite);
-        super.setNickName('B');
-
-
-    }
-    public boolean isValidMove(int newRank, int newFile, Piece[][] board){
-        int rankDifference = Math.abs(newRank - getRank());
-        int fileDifference = Math.abs(newFile - getFile());
-        
-        //Bishop can only move perfectly diagnal. 
-        if(rankDifference != fileDifference){
-            return false;
-        }
-        int rankDirection = getRank() > newRank ? -1 : 1;
-        int fileDirection = getFile() > newFile ? -1 : 1;
-        for (int i = 1; i < rankDifference; i ++){
-            int intermediateRank = getRank() + (i * rankDirection );
-            int intermediateFile = getFile() + (i * fileDirection);
-            if ( !(board[intermediateRank][intermediateFile] instanceof EmptySquare)){
-                System.out.println("No Empty Square at Rank: " + intermediateRank + " File: " + intermediateFile);
-                return false;
-            }
-        }
-        return true;
-    }
-    public boolean move(int newRank, int newFile, Piece[][] board){
-        if(isValidMove(newRank, newFile, board)){
-            setRank(newRank);
-            setFile(newFile);
-            return true;
-        }
-        return false;
-    }
-    public boolean isValidCapture(int newRank, int newFile, Piece[][] board){
-        if(board[newRank][newFile].isWhite() == isWhite()){
-            return false;
-        }
-        int rankDifference = Math.abs(newRank - getRank());
-        int fileDifference = Math.abs(newFile - getFile());
-        if(rankDifference != fileDifference){
-            return false;
-        }
-        int rankDirection = getRank() > newRank ? -1 : 1;
-        int fileDirection = getFile() > newFile ? -1 : 1;
-        //can always capture one space away
-        if (rankDifference == 1 && fileDifference == 1){
-            return true;
-        }
-        for (int i = 1; i < rankDifference; i ++){
-            int intermediateRank = getRank() + (i * rankDirection );
-            int intermediateFile = getFile() + (i * fileDirection);
-            if ( !(board[intermediateRank][intermediateFile] instanceof EmptySquare)){
-                System.out.println("No Empty Square at Rank: " + intermediateRank + " File: " + intermediateFile);
-                return false;
-            }
-        }
-        return true;
-    }
-    public boolean capture(int newRank, int newFile, Piece[][] board){
-        if(isValidCapture(newRank, newFile, board)){
-            setRank(newRank);
-            setFile(newFile);
-            return true;
-        }
-        
-        return false;
-    }
-    
-    @Override
-    public char getNickName(){
-       return nickName;
-    }
-    @Override
-    public String toString(){
-        return "Bishop";
-    }
-
-    @Override
-    public int getPieceValue(){
-        int [][] whiteValueAdjustment =
+    //adjustments based on where the piece is located
+    int [][] whiteValueAdjustment =
         {   
             {-20,-10,-10,-10,-10,-10,-10,-20},
             {-10,  0,  0,  0,  0,  0,  0,-10},
@@ -119,6 +30,103 @@ public class Bishop extends Piece{
             {-10,  0,  0,  0,  0,  0,  0,-10},
             {-20,-10,-10,-10,-10,-10,-10,-20}
         };
+
+    public Bishop(){
+    }
+    public Bishop(Bishop otherBishop){
+        setFile(otherBishop.getFile());
+        setRank(otherBishop.getRank());
+        setColor(otherBishop.isWhite());
+        setNickName('B');
+    }
+
+    public Bishop (int initialRank, int initialFile, boolean isWhite){
+        super(initialRank, initialFile, isWhite);
+        super.setNickName('B');
+    }
+
+    public boolean isValidMove(int newRank, int newFile, Piece[][] board){
+        int rankDifference = Math.abs(newRank - getRank());
+        int fileDifference = Math.abs(newFile - getFile());
+        
+        //Bishop can only move perfectly diagnal. 
+        if(rankDifference != fileDifference){
+            return false;
+        }
+        int rankDirection = getRank() > newRank ? -1 : 1;
+        int fileDirection = getFile() > newFile ? -1 : 1;
+        //looks for any pieces between current square and destination square
+        for (int i = 1; i < rankDifference; i ++){
+            int intermediateRank = getRank() + (i * rankDirection );
+            int intermediateFile = getFile() + (i * fileDirection);
+            if ( !(board[intermediateRank][intermediateFile] instanceof EmptySquare)){
+                System.out.println("No Empty Square at Rank: " + intermediateRank + " File: " + intermediateFile);
+                return false;
+            }
+        }
+        //nothing found in between - we can move there
+        return true;
+    }
+
+    public boolean move(int newRank, int newFile, Piece[][] board){
+        if(isValidMove(newRank, newFile, board)){
+            setRank(newRank);
+            setFile(newFile);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidCapture(int newRank, int newFile, Piece[][] board){
+        //can't capture own piece
+        if(board[newRank][newFile].isWhite() == isWhite()){
+            return false;
+        }
+        int rankDifference = Math.abs(newRank - getRank());
+        int fileDifference = Math.abs(newFile - getFile());
+        if(rankDifference != fileDifference){
+            return false;
+        }
+        int rankDirection = getRank() > newRank ? -1 : 1;
+        int fileDirection = getFile() > newFile ? -1 : 1;
+        //can always capture one space away
+        if (rankDifference == 1 && fileDifference == 1){
+            return true;
+        }
+        //look for any piece in between start and ending square
+        for (int i = 1; i < rankDifference; i ++){
+            int intermediateRank = getRank() + (i * rankDirection );
+            int intermediateFile = getFile() + (i * fileDirection);
+            if ( !(board[intermediateRank][intermediateFile] instanceof EmptySquare)){
+                System.out.println("No Empty Square at Rank: " + intermediateRank + " File: " + intermediateFile);
+                return false;
+            }
+        }
+        //nothing in between - valid move
+        return true;
+    }
+
+    public boolean capture(int newRank, int newFile, Piece[][] board){
+        if(isValidCapture(newRank, newFile, board)){
+            setRank(newRank);
+            setFile(newFile);
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public char getNickName(){
+       return nickName;
+    }
+    @Override
+    public String toString(){
+        return "Bishop";
+    }
+
+    @Override
+    public int getPieceValue(){
+        
         if(isWhite()){
             return 320 + whiteValueAdjustment[getRank()][getFile()];
         }
@@ -128,6 +136,7 @@ public class Bishop extends Piece{
     @Override 
     public List<MoveRequest> getPossibleMoves(Piece[][] board){
         List<MoveRequest> moves = new ArrayList<>();
+        //matrix of the ways the bishop can move
         int[] dRank = {1,1,-1,-1};
         int[] dFile = {1,-1,1,-1};
         for(int i = 0; i < 4; i++){
@@ -135,6 +144,7 @@ public class Bishop extends Piece{
             int newRank = getRank() + dRank[i];
             int newFile = getFile() + dFile[i];
             boolean canContinue = true;
+            //move in that direction and add each legal move iteratively 
             while (canContinue){
                 if(outOfBounds(newRank,newFile)){
                     canContinue = false;
--- src/test/java/erikbissell/com/WebChess/WebChessApplicationTests.java
+++ src/test/java/erikbissell/com/WebChess/WebChessApplicationTests.java
@@ -3,26 +3,45 @@ package erikbissell.com.WebChess;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebChessApplicationTests {
+	
+	@Autowired
+    private MockMvc mockMvc;
+
+	@Test
+	void getChessBoard() throws Exception{
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/chess/board"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	}
 
 	@Test
 	void contextLoads() {
 	}
 	//Tests for Pawn
 	@Test
-	void pawnMoveThreeSpacesReturnsFalse(){
+	void pawn_MoveThreeSpaces_NotValid(){
 		Chessboard chessboard = new Chessboard();
 		assertFalse(chessboard.movePiece(6,0,3,0));
 	}
 	@Test
-	void pawnMoveTwoSpacesReturnsTrue(){
+	void pawn_MoveTwoSpaces_Valid(){
 		Chessboard chessboard = new Chessboard();
 		assertTrue(chessboard.movePiece(6,0,4,0));
 
 	}
+	
 
 }
